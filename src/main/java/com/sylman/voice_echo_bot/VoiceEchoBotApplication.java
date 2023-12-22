d6ec9e89e7506aefeafc8833ad2c4c21a4f93f02
Lyubov <l.musilmanbekova12@gmail.com>
2023-12-21 18:03:44 -0700

    This is a combination of 2 commits. To avoid the error of downloading a large file.

--- src/main/java/com/sylman/voice_echo_bot/VoiceEchoBotApplication.java
+++ src/main/java/com/sylman/voice_echo_bot/VoiceEchoBotApplication.java
@@ -1,39 +1,37 @@
 package com.sylman.voice_echo_bot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.SpeechResult;
 import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
-@SpringBootApplication
 public class VoiceEchoBotApplication {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         Configuration configuration = new Configuration();
 
-        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration.setAcousticModelPath("russianModels/ru.lm.bin");
         configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
         configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
+//        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+//        recognizer.startRecognition(true);
         StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
-        recognizer.startRecognition(new FileInputStream("hello2.wav"));
+        recognizer.startRecognition(new FileInputStream("woman.wav"));
 
-//        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
-//        InputStream stream = new FileInputStream(new File("hello2.wav"));
+        System.out.println("Recognition beginning...");
 
-//        recognizer.startRecognition(stream);
 
         SpeechResult result;
         while ((result = recognizer.getResult()) != null) {
-            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+            String transcription = result.getHypothesis();
+            System.out.println("Transription: " + transcription);
         }
         recognizer.stopRecognition();
     }
 }
+
+
+
+
+
