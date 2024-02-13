e3a373fab3399670ff575d4ff84d48acf76a782e
Lyubov <l.musilmanbekova12@gmail.com>
2024-02-13 22:34:25 +0300

    The VoiceEchoBotApplication class has been modified. The paths to the dictionary/library/acoustic model have been reverted back ti their original state.

--- src/main/java/com/sylman/voice_echo_bot/VoiceEchoBotApplication.java
+++ src/main/java/com/sylman/voice_echo_bot/VoiceEchoBotApplication.java
@@ -3,6 +3,7 @@ package com.sylman.voice_echo_bot;
 import java.io.FileInputStream;
 
 import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import edu.cmu.sphinx.api.SpeechResult;
 import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
@@ -11,14 +12,14 @@ public class VoiceEchoBotApplication {
         Configuration configuration = new Configuration();
 
         // TODO: 12/26/2023 the path are laid incorrectly, the project is under development 
-        configuration.setAcousticModelPath("russianModels/ru.lm.bin");
+        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
         configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
         configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
-//        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
-//        recognizer.startRecognition(true);
-        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
-        recognizer.startRecognition(new FileInputStream("woman.wav"));
+        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+        recognizer.startRecognition(true);
+//        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
+//        recognizer.startRecognition(new FileInputStream("woman.wav"));
         // TODO: 12/26/2023 path to voice woman.wav is real. audio is not pushed into gitHub 
 
         System.out.println("Recognition beginning...");
